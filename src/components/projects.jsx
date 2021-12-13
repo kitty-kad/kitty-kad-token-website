e7045814eda41cb6f8ed6c26cdb1beedc758000a
kitty-kad <kitty-kad>
2021-12-13 15:13:41 +0000

    Added projects link

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,13 +8,15 @@ export const Projects = (props) => {
         <div className='row'>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-sm-6 col-md-3'>
-                  {' '}
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
-                </div>
-              ))
+              <div key={`${d.title}-${i}`} className='col-sm-6 col-md-3'>
+                {' '}
+                <i className={d.icon}></i>
+                {d.link != null ? 
+                <a href={d.link}> <h3 style={{textDecoration: 'underline'}}>{d.title}</h3> </a>
+                : <h3>{d.title}</h3>}
+                <p>{d.text}</p>
+              </div>
+            ))
             : 'Loading...'}
         </div>
       </div>
